5c03d898504ad769a6984a0ecd330a14882e95e7
zhenquan.qiu <gnsyxiang@163.com>
2023-08-18 10:10:54 +0800

    build: only for vim prompt

--- build-script/stm32/stm32h7xx/config.sh
+++ build-script/stm32/stm32h7xx/config.sh
@@ -11,7 +11,7 @@ cross_gcc_path=/opt/toolchains/mcu/gcc-arm-none-eabi-6_2-2016q4/bin/arm-none-eab
 
 configure_param=
 
-cppflag=-mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections
+cppflag=-mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -I/opt/toolchains/mcu/gcc-arm-none-eabi-6_2-2016q4/arm-none-eabi/include
 cflag=-Dgcc -std=c99 -DUSE_HAL_DRIVER -DSTM32H743xx
 cxxflag=
 ldflag=-mcpu=cortex-m7 -mthumb -mfpu=fpv5-d16 -mfloat-abi=hard -specs=nano.specs -specs=nosys.specs
